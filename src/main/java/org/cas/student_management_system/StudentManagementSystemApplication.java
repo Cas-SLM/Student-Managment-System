b0e09a0480b66e5f773e49c9dc9ed8b601de04fb
smagwentshu023 <smagwentshu99@gmail.com>
2024-09-18 17:02:42 +0200

    Spring Database connection information, to be revised

--- src/main/java/org/cas/student_management_system/StudentManagementSystemApplication.java
+++ src/main/java/org/cas/student_management_system/StudentManagementSystemApplication.java
@@ -2,10 +2,12 @@ package org.cas.student_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // Makes the class serve rest endpoints
 public class StudentManagementSystemApplication {
-
+    // Port is set by automatically check Tomcat log
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
     }
